79a136c6a3fe0db70d081e5a18c0993c4b9c7e7c
Nitin Ankad <46038298+nitinankad@users.noreply.github.com>
2024-01-30 18:43:40 -0600

    Add more animal names

--- server/src/utils/stringHelpers.ts
+++ server/src/utils/stringHelpers.ts
@@ -4,7 +4,20 @@ export const generateId = (): string => {
   return uuidv4();
 };
 
-export const anonymousAnimals = ["Alligator","Anteater","Armadillo","Auroch","Axolotl","Badger","Bat","Bear","Beaver","Buffalo","Camel","Capybara","Chameleon","Cheetah","Chinchilla","Chipmunk","Chupacabra","Cormorant","Coyote","Crow","Dingo","Dinosaur","Dog","Dolphin","Duck","Elephant","Ferret","Fox","Frog","Giraffe","Gopher","Grizzly","Hedgehog","Hippo","Hyena","Ibex","Ifrit","Iguana","Jackal","Kangaroo","Koala","Kraken","Lemur","Leopard","Liger","Lion","Llama","Loris","Manatee","Mink","Monkey","Moose","Narwhal","Nyan Cat","Orangutan","Otter","Panda","Penguin","Platypus","Pumpkin","Python","Quagga","Rabbit","Raccoon","Rhino","Sheep","Shrew","Skunk","Squirrel","Tiger","Turtle","Walrus","Wolf","Wolverine","Wombat"];
+export const anonymousAnimals = [
+  "Alligator", "Anteater", "Armadillo", "Auroch", "Axolotl", "Badger", "Bat", "Bear", "Beaver",
+  "Buffalo", "Camel", "Capybara", "Chameleon", "Cheetah", "Chinchilla", "Chipmunk", "Chupacabra",
+  "Cormorant", "Coyote", "Crow", "Dingo", "Dinosaur", "Dog", "Dolphin", "Duck", "Elephant",
+  "Ferret", "Fox", "Frog", "Giraffe", "Gopher", "Grizzly", "Hedgehog", "Hippo", "Hyena", "Ibex",
+  "Iguana", "Jackal", "Kangaroo", "Koala", "Kraken", "Lemur", "Leopard", "Liger", "Lion",
+  "Llama", "Loris", "Manatee", "Mink", "Monkey", "Moose", "Narwhal", "Nyan Cat", "Orangutan",
+  "Otter", "Panda", "Penguin", "Platypus", "Pumpkin", "Python", "Quagga", "Rabbit", "Raccoon",
+  "Rhino", "Sheep", "Shrew", "Skunk", "Squirrel", "Tiger", "Turtle", "Walrus", "Wolf", "Wolverine",
+  "Wombat", "Zebra", "Alpaca", "Bison", "Caribou", "Cougar", "Crane", "Deer", "Eagle", "Falcon",
+  "Flamingo", "Gazelle", "Hamster", "Heron", "Jaguar", "Lynx", "Mandrill", "Meerkat", "Ocelot",
+  "Pangolin", "Peacock", "Pelican", "Quokka", "Raven", "Reindeer", "Salamander", "Seal", "Sloth",
+  "Swan", "Tapir", "Uakari", "Vulture", "Warthog", "Weasel", "Yak", "Zebu"
+];
 
 export const getRandomElement = (arr: Array<string>) => {
   const randomIndex = Math.floor(Math.random() * arr.length);
